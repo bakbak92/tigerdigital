7f3adcf1cf362b878929b54a83b4236d5f081a76
Bakary <bakary.coly@echoline.fr>
2021-05-22 16:56:15 +0200

    start

---
+++ layouts/default.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item is-primary logo">Tiger Digital</a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+
+        <div class="navbar-end">
+          <a class="navbar-item">
+            Nos services
+          </a>
+          <a class="navbar-item">
+            Qui sommes nous
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container column is-10">
+      <nuxt />
+    </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+          is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        </p>
+      </div>
+    </footer>
+
+  </div>
+  
+</template>
+<style>
+.logo{
+  font-size: 18px;
+  font-weight: 600;
+  color: #7958d5;
+}
+.center{
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <div class="column presentation">
+        <h1 class="title">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum quod
+        </h1>
+        <p class="subtitle">
+          Officiis laboriosam repudiandae, quaerat culpa minus similique nihil ipsum tenetur itaque nulla in dicta quibusdam nemo sit incidunt possimus eveniet.
+        </p>
+         <button class="button is-primary">
+           Call to Action
+         </button>
+      </div>
+      <div class="column">
+        <img src="../assets/head-social.png" alt="">
+      </div>
+    </div>
+    <h2 class="title center">
+      Nos services
+    </h2>
+    <div class="columns is-mobile">
+      <div class="card">
+        <div class="card-content">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="../assets/code.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="content">
+            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-content">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="../assets/social.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="content">
+            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-content">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="../assets/business.png" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="content">
+            Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+    <div class="container">
+      <h2 class="title center">Contactez-nous</h2>
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Text input">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link">Submit</button>
+        </div>
+        <div class="control">
+          <button class="button is-link is-light">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
+<style>
+.presentation{
+  text-align: center;
+}
+.columns .card:first-child{
+  margin-right: 32px;
+}
+.columns .card{
+  margin-bottom: 32px;
+}
+.columns .card:last-child{
+  margin-left: 32px;
+}
+.title{
+  margin-top: 32px;
+}
+</style>
